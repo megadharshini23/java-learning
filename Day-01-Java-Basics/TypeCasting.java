4d4c2dda2f95a5d2b1cd1b50a33e9d31b2f3ed31
Mega Dharshini <megailayaraja@gmail.com>
2026-07-14 23:00:25 +0530

    Day 1: Java Basics

---
+++ Day-01-Java-Basics/TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        int number = 100;
+        double value = number;
+
+        System.out.println(value);
+
+        double pi = 3.14;
+        int x = (int) pi;
+
+        System.out.println(x);
+    }
+}
