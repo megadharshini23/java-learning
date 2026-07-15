ee7f6c6bbf73c15afd3d405d8519eb1f3d211c2c
Mega Dharshini <megailayaraja@gmail.com>
2026-07-15 09:28:14 +0530

    Day 1: Java Basics

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        String name = "Mega";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
